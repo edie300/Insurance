7ba63875e70ca117c9cdc7dca1a4d6d53b24165f
1 <a1@1deMacBook-Pro.lan>
2020-06-19 02:37:04 +0200

    Updates

---
+++ precache-manifest.9fff4b15582debedc2acd41ab602acb2.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbad809e103965285f60e7c933c50a7",
+    "url": "/Insurance/index.html"
+  },
+  {
+    "revision": "03a9b4d4695d2539dadc",
+    "url": "/Insurance/static/css/2.7be311a7.chunk.css"
+  },
+  {
+    "revision": "1546f8424c89365752ac",
+    "url": "/Insurance/static/css/main.38be9fba.chunk.css"
+  },
+  {
+    "revision": "03a9b4d4695d2539dadc",
+    "url": "/Insurance/static/js/2.859b6c44.chunk.js"
+  },
+  {
+    "revision": "1ed02bcb1366cf3c9e434a961f02910f",
+    "url": "/Insurance/static/js/2.859b6c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1546f8424c89365752ac",
+    "url": "/Insurance/static/js/main.94a7a04b.chunk.js"
+  },
+  {
+    "revision": "b4a6ec1c41aae21f370f",
+    "url": "/Insurance/static/js/runtime-main.35305cfd.js"
+  },
+  {
+    "revision": "11fbcc47738cc7cb86ae339072f39c20",
+    "url": "/Insurance/static/media/1.11fbcc47.jpg"
+  },
+  {
+    "revision": "5a567a6e4eea505355c24921201c0e3f",
+    "url": "/Insurance/static/media/2.5a567a6e.jpg"
+  },
+  {
+    "revision": "45b87ba2572f3cbcd9ebf0d15543c2a9",
+    "url": "/Insurance/static/media/3.45b87ba2.jpg"
+  },
+  {
+    "revision": "c5ce193309e4fe26487cb25e10b44412",
+    "url": "/Insurance/static/media/4.c5ce1933.jpg"
+  },
+  {
+    "revision": "9b313b6383f1a48502d9c543d481f2c4",
+    "url": "/Insurance/static/media/5.9b313b63.jpg"
+  },
+  {
+    "revision": "abe2f0df85c8ec58a0858f66e8270244",
+    "url": "/Insurance/static/media/6.abe2f0df.jpg"
+  },
+  {
+    "revision": "84505fb06234c9892da0bed60b23f735",
+    "url": "/Insurance/static/media/7.84505fb0.jpg"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/Insurance/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/Insurance/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/Insurance/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/Insurance/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/Insurance/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "dec31567f232966eccf0eb73223a30f4",
+    "url": "/Insurance/static/media/avater.dec31567.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Insurance/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Insurance/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Insurance/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Insurance/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Insurance/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "baae1ab59fbe78319792aad7ea20461f",
+    "url": "/Insurance/static/media/funeral.baae1ab5.jpg"
+  },
+  {
+    "revision": "a4a1dd335526d0f690d1826a26a0d3fb",
+    "url": "/Insurance/static/media/home.a4a1dd33.jpg"
+  },
+  {
+    "revision": "32c477b4cc36c7859c702815a9746e8f",
+    "url": "/Insurance/static/media/landingpage.32c477b4.png"
+  },
+  {
+    "revision": "17fcda6dd4691f752031775e44f73b11",
+    "url": "/Insurance/static/media/medical.17fcda6d.jpg"
+  },
+  {
+    "revision": "86e631ea71f4c5571740d804ad616c27",
+    "url": "/Insurance/static/media/motor.86e631ea.jpg"
+  },
+  {
+    "revision": "6a49bc977b0848cbdabe5a7ba1ce2b8d",
+    "url": "/Insurance/static/media/motor1.6a49bc97.jpg"
+  },
+  {
+    "revision": "e4e3d40ffb5044e0f5bb5373e931a647",
+    "url": "/Insurance/static/media/totum.e4e3d40f.png"
+  }
+]);
